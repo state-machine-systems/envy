71291f16d67e9b9aa9dafeb5bea14e0ce313350d
John Wright <john@statemachinesystems.co.uk>
2014-09-30 21:38:45 -0400

    Add utility class for reflection assertions

---
+++ src/main/java/com/statemachinesystems/envy/Assertions.java
@@ -0,0 +1,27 @@
+package com.statemachinesystems.envy;
+
+import java.lang.reflect.Method;
+
+public class Assertions {
+
+    public static void assertInterface(Class<?> c) {
+        if (! c.isInterface()) {
+            throw new IllegalArgumentException(
+                    String.format("%s is not an interface", c.getName()));
+        }
+    }
+
+    public static void assertEnum(Class<?> c) {
+        if (! c.isEnum()) {
+            throw new IllegalArgumentException(
+                    String.format("%s is not an enum", c.getName()));
+        }
+    }
+
+    public static void assertMethodWithNoParameters(Method m) {
+        if (m.getParameterTypes().length > 0) {
+            throw new IllegalArgumentException(
+                    String.format("%s must not take any parameters", m.getName()));
+        }
+    }
+}
---
+++ src/test/java/com/statemachinesystems/envy/AssertionsTest.java
@@ -0,0 +1,47 @@
+package com.statemachinesystems.envy;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+public class AssertionsTest {
+
+    private static enum ExampleEnum {}
+
+    private static interface ExampleInterface {
+        String methodWithNoParameters();
+        String methodWithParameters(String parameter1, String parameter2);
+    }
+
+    @Test
+    public void detectsEnumClass() {
+        Assertions.assertEnum(ExampleEnum.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rejectsNonEnumClass() {
+        Assertions.assertEnum(ExampleInterface.class);
+    }
+
+    @Test
+    public void detectsInterfaceClass() {
+        Assertions.assertInterface(ExampleInterface.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rejectsNonInterfaceClass() {
+        Assertions.assertInterface(ExampleEnum.class);
+    }
+
+    @Test
+    public void detectsMethodWithNoParameters() throws NoSuchMethodException {
+        Method method = ExampleInterface.class.getDeclaredMethod("methodWithNoParameters");
+        Assertions.assertMethodWithNoParameters(method);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rejectsMethodWithParameters() throws NoSuchMethodException {
+        Method method = ExampleInterface.class.getDeclaredMethod("methodWithParameters", String.class, String.class);
+        Assertions.assertMethodWithNoParameters(method);
+    }
+}
