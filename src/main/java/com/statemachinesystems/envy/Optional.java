0a96496bf879cd69b198de5cf156586a59395f86
John Wright <john@statemachinesystems.co.uk>
2014-12-03 21:05:10 +0000

    Add @Optional annotation

---
+++ src/main/java/com/statemachinesystems/envy/Optional.java
@@ -0,0 +1,14 @@
+package com.statemachinesystems.envy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Marks a nullable return value of an accessor method in a configuration interface.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Optional {}
